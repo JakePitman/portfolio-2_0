d109fb44570ee985d11d8870754c814009224bf3
Jay Pitman <jakeseanp@gmail.com>
2018-09-08 18:16:34 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Portfolio 2.0</h1>
+        <h2>Hello world</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
