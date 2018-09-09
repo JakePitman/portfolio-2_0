b537143dd37401976d2b76d960963eeb35230669
Jay Pitman <jakeseanp@gmail.com>
2018-09-09 20:07:29 +1000

    add dynamic color changing to DownIndicator component

--- src/components/DownIndicator.js
+++ src/components/DownIndicator.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
-import {SECTION_BACKGROUND, DEAD_GREY} from '../constants/colors'
+import { connect } from 'react-redux'
+
+import { ABOUT_ME, MY_TECH_JOURNEY, PROJECTS } from '../constants/nav-button-types'
+import { 
+  SECTION_BACKGROUND,
+  DEAD_GREY,
+  ABOUT_ME_MAIN_COLOR,
+  MY_TECH_JOURNEY_MAIN_COLOR,
+  PROJECTS_MAIN_COLOR
+} from '../constants/colors'
+
+//---------------------STYLING-----------------------
 
 const Banner = styled.div`
   width: 100%;
@@ -10,25 +21,48 @@ const Banner = styled.div`
   justify-content: center;
   align-items: center;
 `
+//---------------------COMPONENT---------------------
 
-const DownArrow = styled.i`
-  border: solid ${ DEAD_GREY };
-  border-width: 0 7px 7px 0;
-  font-size: 50px;
-  width: 30px;
-  height: 30px;
-  transform: rotate(45deg);
-  display: inline-block;
-`
+const mapStateToProps = state => {
+  return { currentSection: state.currentSection }
+}
 
 
-const DownIndicator = () => {
+const ConnectedDownIndicator = ({ currentSection }) => {
+
+  let mainColor;
+  switch (currentSection) {
+    case ABOUT_ME:
+      mainColor = ABOUT_ME_MAIN_COLOR
+      break;
+    case MY_TECH_JOURNEY:
+      mainColor = MY_TECH_JOURNEY_MAIN_COLOR
+      break;
+    case PROJECTS:
+      mainColor = PROJECTS_MAIN_COLOR
+      break;
+    default:
+      mainColor = DEAD_GREY
+  }
+
+// using styles object to make dynamically changing color easier
+const downArrowStyles = {
+  borderRight: `solid ${mainColor}`,
+  borderBottom: `solid ${mainColor}`,
+  borderWidth: '0 3px 3px 0',
+  width: '30px',
+  height: '30px',
+  transform: 'rotate(45deg)',
+  display: 'inline-block'
+}
 
   return (
     <Banner> 
-     <p><DownArrow/></p>
+     <p><i style={downArrowStyles} /></p>
     </Banner>
   )
 }
+
+const DownIndicator = connect(mapStateToProps) (ConnectedDownIndicator)
 
 export default DownIndicator
