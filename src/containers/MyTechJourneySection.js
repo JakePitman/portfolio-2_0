cbc29de2241c26025871e5cfb2689e6ccbd540f6
Jay Pitman <jakeseanp@gmail.com>
2018-09-17 00:53:55 +1000

    resize coming-soon messages

--- src/containers/MyTechJourneySection.js
+++ src/containers/MyTechJourneySection.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
-import { MY_TECH_JOURNEY_MAIN_COLOR, DEAD_GREY } from '../constants/colors'
-import { TOP_SECTION_FONT, MAIN_PARAGRAPH_FONT } from '../constants/fonts'
-import { PHONE_BREAKPOINT } from '../constants/media-queries'
+import WorkInProgressSign from '../components/WorkInProgressSign';
+import { MY_TECH_JOURNEY_MAIN_COLOR, DEAD_GREY } from '../constants/colors';
+import { TOP_SECTION_FONT, MAIN_PARAGRAPH_FONT } from '../constants/fonts';
+import { PHONE_BREAKPOINT } from '../constants/media-queries';
 
 //------------------STYLING------------------
 
 const MainColorText = styled.span`
-  color: ${ MY_TECH_JOURNEY_MAIN_COLOR }
-  font-family: ${ TOP_SECTION_FONT }
-`
+  color: ${MY_TECH_JOURNEY_MAIN_COLOR}
+  font-family: ${TOP_SECTION_FONT}
+`;
 const ComingSoon = styled.h1`
   font-size: 1.5rem;
   font-family: ${MAIN_PARAGRAPH_FONT};
@@ -20,18 +21,18 @@ const ComingSoon = styled.h1`
   display: flex;
   justify-content: space-around;
   align-items: center;
-  color: ${DEAD_GREY}
-
-  @media ( min-width: ${ PHONE_BREAKPOINT } ) {
+  color: ${DEAD_GREY};
+  @media (min-width: ${PHONE_BREAKPOINT}) {
     font-size: 100px;
   }
-`
+`;
 
 const MyTechJourneySection = () => {
-
   return (
-    <ComingSoon><MainColorText>My Tech Journey</MainColorText> is coming soon!</ComingSoon>
-  )
-}
+    <ComingSoon>
+      <MainColorText>My Tech Journey</MainColorText> is coming soon!
+    </ComingSoon>
+  );
+};
 
-export default MyTechJourneySection
+export default MyTechJourneySection;
