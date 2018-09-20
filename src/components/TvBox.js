92af8a383a3563768319306640736968926476db
Jay Pitman <jakeseanp@gmail.com>
2018-09-20 16:08:38 +1000

    remove profile picture, replace with dot points

--- src/components/TvBox.js
+++ src/components/TvBox.js
@@ -1,37 +1,38 @@
-import React, {Fragment} from 'react';
-import styled, {keyframes} from 'styled-components'
-import { connect } from 'react-redux'
+import React, { Fragment } from 'react';
+import styled, { keyframes } from 'styled-components';
+import { connect } from 'react-redux';
 
-import profilePicture from '../assets/img/profile.png'
-import { ABOUT_ME_MAIN_COLOR } from '../constants/colors'
-import { ABOUT_ME, MY_TECH_JOURNEY, PROJECTS } from '../constants/nav-button-types'
-import { TOP_SECTION_FONT } from '../constants/fonts'
-import { PHONE_BREAKPOINT } from '../constants/media-queries'
-
+import { ABOUT_ME_MAIN_COLOR } from '../constants/colors';
+import {
+  ABOUT_ME,
+  MY_TECH_JOURNEY,
+  PROJECTS
+} from '../constants/nav-button-types';
+import { TOP_SECTION_FONT, SUBHEADING_FONT } from '../constants/fonts';
+import { PHONE_BREAKPOINT } from '../constants/media-queries';
+import getMainColor from '../utilities/get-main-color';
 
 //-------------------------------STYLING-------------------------
 
 const Container = styled.div`
   display: flex;
   justify-content: center;
-`
+`;
 
 const Frame = styled.div`
   background-color: transparent;
   border-radius: 2rem;
-  box-shadow: inset 0 0 2vw black,
-    inset 0 0 3rem black,
-    0 0 10rem black;
+  box-shadow: inset 0 0 2vw black, inset 0 0 3rem black, 0 0 10rem black;
   width: 80vw;
   height: 40vh;
   position: relative;
   margin: 2rem 0;
 
-  @media (min-width: ${ PHONE_BREAKPOINT }) {
+  @media (min-width: ${PHONE_BREAKPOINT}) {
     width: 500px;
     height: 50vh;
   }
-`
+`;
 
 const Output = styled.div`
   background-color: rgba(35, 44, 77);
@@ -41,7 +42,7 @@ const Output = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-`
+`;
 
 const scanlinesAnimation = keyframes`
   0% {
@@ -72,7 +73,7 @@ const scanlinesAnimation = keyframes`
             rgba(0,0,0,0.2) 3px,
             rgba(0,0,0,0.6) 4px);
   }
-`
+`;
 
 const Scanlines = styled.div`
   animation: ${scanlinesAnimation} 8s infinite;
@@ -83,7 +84,7 @@ const Scanlines = styled.div`
   position: absolute;
   left: -1rem;
   top: -1rem;
-`
+`;
 
 const glowAnimation = keyframes`
   0% { opacity: 0.1; }
@@ -91,52 +92,53 @@ const glowAnimation = keyframes`
   50% { opacity: 0.1; }
   75% { opacity: 0.4; }
   100% { opacity: 0.1; }
-`
+`;
 const Glow = styled.div`
   animation: ${glowAnimation} 20s infinite;
-  background:
-    radial-gradient(circle at center,
-    rgba(120,120,210,0.8) 0%,
-    rgba(90,90,150,0.68) 58%,
-    rgba(90,90,140,0.47) 80%,
-    rgba(200,200,255,0.27) 93%,
-    rgba(100,100,155,0) 100%);
+  background: radial-gradient(
+    circle at center,
+    rgba(120, 120, 210, 0.8) 0%,
+    rgba(90, 90, 150, 0.68) 58%,
+    rgba(90, 90, 140, 0.47) 80%,
+    rgba(200, 200, 255, 0.27) 93%,
+    rgba(100, 100, 155, 0) 100%
+  );
   opacity: 0.15;
   position: absolute;
   width: 100%;
   height: 100%;
   top: 0;
   left: 0;
-`
+`;
 
 const nameShadowAnimation = keyframes`
   0% { text-shadow: -3px 0.1rem 1rem #004F68 }
   50% { text-shadow: 3px 0.2rem 2rem #004f68 }
   100% { text-shadow: -3px 0.1rem 3rem #004f68 }
-`
+`;
 
 const namePaddingAnimation = keyframes`
   0% { padding: 2px 0 0 1px; }
   10% { padding: 0px 0 0 0; }
-`
+`;
 const Name = styled.div`
   animation: ${nameShadowAnimation} 0.5s infinite;
   animation: ${namePaddingAnimation} 1.5s infinite;
-  font-family: ${ TOP_SECTION_FONT };
+  font-family: ${TOP_SECTION_FONT};
   color: ${ABOUT_ME_MAIN_COLOR};
   font-size: 50px;
   text-align: center;
-`
+`;
 
 const flickerAnimation = keyframes`
   0%, 17%, 80% { opacity: 0.4; }
   15% , 30%, 60%, 85%, 95% { opacity: 0; }
-`
+`;
 
 const Flicker = styled.div`
   animation: ${flickerAnimation} 2s infinite;
   position: absolute;
-  font-family: ${ TOP_SECTION_FONT };
+  font-family: ${TOP_SECTION_FONT};
   color: rgba(113, 218, 252, 0.3);
   font-size: 50px;
   text-align: center;
@@ -144,59 +146,68 @@ const Flicker = styled.div`
   margin-right: auto;
   padding-bottom: 20px;
   padding-right: 10px;
-`
+`;
 
-const Image = styled.img`
-  width: 100%;
-  height: 100%;
-`
+const SectionTextList = styled.ul``;
 
 //----------------------COMPONENT--------------------------
 
-const mapStateToProps = state => {
-  return { currentSection: state.currentSection }
-}
+const mapStateToProps = (state) => {
+  return { currentSection: state.currentSection };
+};
 
 const ConnectedTvBox = ({ currentSection }) => {
+  const SectionText = styled.li`
+    color: white;
+    font-family: ${SUBHEADING_FONT};
+    text-shadow: 0 0 20px ${getMainColor(currentSection)};
+    margin: 20px;
+    font-size: 50px;
+    list-style: none;
+  `;
 
-  let outputJsx
-  switch ( currentSection ) {
+  let outputJsx;
+  switch (currentSection) {
     case ABOUT_ME:
       outputJsx = (
-        <Image src={profilePicture}/>
-      )
+        <SectionTextList>
+          <SectionText>> Who am I? </SectionText>
+          <SectionText>> Tech Stacks </SectionText>
+          <SectionText>> Interests </SectionText>
+        </SectionTextList>
+      );
       break;
     case MY_TECH_JOURNEY:
       outputJsx = (
-        <Fragment>
-          <Name>Jake Pitman</Name> 
-          <Flicker>Jake Pitman</Flicker>
-        </Fragment>
-      )
+        <SectionTextList>
+          <SectionText>> Code Camp</SectionText>
+          <SectionText>> Coder Academy</SectionText>
+          <SectionText>> Internship </SectionText>
+        </SectionTextList>
+      );
       break;
     case PROJECTS:
       outputJsx = (
-        <Fragment>
-          <Name>Jake Pitman</Name> 
-          <Flicker>Jake Pitman</Flicker>
-        </Fragment>
-      )
+        <SectionTextList>
+          <SectionText>> Portfolio 1.0</SectionText>
+          <SectionText>> Artistree </SectionText>
+          <SectionText>> Social Banana </SectionText>
+        </SectionTextList>
+      );
       break;
     default:
       outputJsx = (
         <Fragment>
-          <Name>Jake Pitman</Name> 
+          <Name>Jake Pitman</Name>
           <Flicker>Jake Pitman</Flicker>
         </Fragment>
-      )
+      );
   }
 
   return (
     <Container>
       <Frame>
-        <Output>
-          {outputJsx}
-        </Output>
+        <Output>{outputJsx}</Output>
         <Scanlines />
         <Glow />
       </Frame>
@@ -204,6 +215,6 @@ const ConnectedTvBox = ({ currentSection }) => {
   );
 };
 
-const TvBox = connect(mapStateToProps) (ConnectedTvBox)
+const TvBox = connect(mapStateToProps)(ConnectedTvBox);
 
 export default TvBox;
